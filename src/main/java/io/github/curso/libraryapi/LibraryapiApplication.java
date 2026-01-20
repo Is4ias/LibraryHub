92cb17c87c4177a18f71a9984cf3205c81e0055b
Is4ias <isaias02dev@gmail.com>
2026-01-20 17:38:10 -0300

    feat: operação de persistencia com Repositorio

--- src/main/java/io/github/curso/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/curso/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,32 @@
 package io.github.curso.libraryapi;
 
+import io.github.curso.libraryapi.model.Autor;
+import io.github.curso.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryapiApplication.class, args);
+		var context = SpringApplication.run(LibraryapiApplication.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+
+		exemploSalvarRegistro(repository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository){
+		Autor autor = new Autor();
+		autor.setNome("José");
+		autor.setNacionalidade("Brasileiro");
+		autor.setDataNascimento(LocalDate.of(1950, 1, 31));
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("autor salvo:" + autorSalvo);
+
+
 	}
 
 }
