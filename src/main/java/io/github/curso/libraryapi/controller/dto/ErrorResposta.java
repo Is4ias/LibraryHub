2e6e8b2eb6cae49ecd3cb3c7202ad441952797c1
Is4ias <isaias02dev@gmail.com>
2026-04-07 15:24:05 -0300

    feat: padronizando as respostas de erro com DTO

---
+++ src/main/java/io/github/curso/libraryapi/controller/dto/ErrorResposta.java
@@ -0,0 +1,21 @@
+package io.github.curso.libraryapi.controller.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+
+
+public record ErrorResposta(int status, String mensagem, List<ErroCampo> erros) {
+
+    public static ErrorResposta resostaPadrao(String mensagem){
+        return  new ErrorResposta(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
+    }
+
+    public static ErrorResposta conflito(String mensagem){
+        return new ErrorResposta(HttpStatus.CONFLICT.value(), mensagem, List.of());
+
+    }
+
+
+}
