535d4375292fd48416e1eaae8a411f2dd0cb1b3d
Is4ias <isaias02dev@gmail.com>
2026-07-10 14:07:47 -0300

    feat: aprimorando trativas de erro 403 FORBIDEN

--- src/main/java/io/github/curso/libraryapi/controller/commom/GlobalExceptionHandler.java
+++ src/main/java/io/github/curso/libraryapi/controller/commom/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import io.github.curso.libraryapi.exceptions.OperacaoNaoPermitida;
 import io.github.curso.libraryapi.exceptions.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -55,6 +56,15 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
                 "Error de validação.",
                 List.of(new ErroCampo(e.getCampo(), e.getMessage()))
+        );
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResposta hendleAcessDeniedException(AccessDeniedException e){
+        return new ErrorResposta(
+                HttpStatus.FORBIDDEN.value(),
+                "Acesso negado!!", List.of()
         );
     }
 
